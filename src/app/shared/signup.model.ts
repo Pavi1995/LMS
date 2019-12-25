6e2b2033f25cabbf58026611ef9d32d0f0d9840b
Pavithran <pavithran001ramesh@gmail.com>
2019-12-25 11:14:08 +0530

    app

---
+++ src/app/shared/signup.model.ts
@@ -0,0 +1,9 @@
+export class SignUp {
+  constructor(
+    public firstname: string,
+    public lastname: string,
+    public email: string,
+    public password: string,
+    public retypepassword: string
+  ) {}
+}
